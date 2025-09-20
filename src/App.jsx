26f73b44bcd38540f97da139304655ec0ac267fb
lika-ruby <mary.havrylova.2011@gmail.com>
2025-09-20 12:19:18 +0300

    makes all

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,30 @@ class App extends Component {
     filter: "",
   };
 
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+    }
+  }
+
+  componentDidMount() {
+    if (localStorage.getItem("contacts")) {
+      console.log("a");
+      this.setState({ contacts: JSON.parse(localStorage.getItem("contacts")) });
+    } else {
+      console.log("b");
+      localStorage.setItem(
+        "contacts",
+        JSON.stringify([
+          { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+          { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+          { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+          { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+        ])
+      );
+    }
+  }
+
   setFilter = (value) => {
     this.setState({ filter: value });
   };
